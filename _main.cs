ab3bb9c8ff82bbe46005d402812bc5470f2a35ed
ALEXCNB-G3\Alex <ksyjkh24@nate.com>
2017-05-17 16:16:14 +0900

    fixed #5 main 메소드 작성

---
+++ _main.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JHJFramework.Controls
+{
+	public class KkogHaeyahana
+	{
+		public void main()
+		{
+			//여기가 시작점인가??
+		}
+	}
+}
