b5f58f429a6c5554e38ff23b9d282e5e5d428ede
ALEXCNB-G3\Alex <ksyjkh24@nate.com>
2017-05-17 15:58:14 +0900

    fixed #3 div 작성

---
+++ div.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using JHJFramework.Common;
+
+namespace JHJFramework.Controls
+{
+	public class KkogHaeyahana
+	{
+		public void div()
+		{
+			//¹» ³ª´²¾ß ÇÏÁö??
+		}
+	}
+}
